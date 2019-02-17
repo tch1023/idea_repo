61f79c0147a64a90886ef31c32621888ae1d2763
admin <845656662@qq.com>
2019-02-17 18:24:17 +0800

    git_idea测试

---
+++ git_test/src/cn/itcast/GitTset.java
@@ -0,0 +1,11 @@
+package cn.itcast;
+
+/**
+ * @Author: tch
+ * @Date: 2019/2/17 下午 05:00
+ */
+public class GitTset {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
